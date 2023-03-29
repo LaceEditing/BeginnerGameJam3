884d2d124855029f863343916b714a6c470be420
Lace <124857757+LaceEditing@users.noreply.github.com>
2023-03-29 16:01:31 -0500

    clean up code and optimized pause menu

--- BeginnerGameJam3/Assets/Scripts/GameManager.cs
+++ BeginnerGameJam3/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 using TMPro;
 
 
@@ -11,10 +12,8 @@ public class GameManager : MonoBehaviour
 
     [Header("Menu Components")]
     public bool isPaused = false;
-    public GameObject currentMenu; //Tracks current menu to toggle
-    public GameObject pauseMenu;
+    public GameObject pauseUI;
 
-    //Load screen ?
     [Header("----- Scene Transition Components -----")]
     private AsyncOperation operation; //for loading a scene
 
@@ -23,44 +22,47 @@ public class GameManager : MonoBehaviour
         instance = this;
 
     }
-    // Start is called before the first frame update
+
     void Start()
     {
-        currentMenu = pauseMenu;
-
         Time.timeScale = 1;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        //Check for pause menu button/s
-            //Will have to add checks for what state the game is in
-            //As well as controller input
         if(Input.GetButtonDown("Esc") )
         {
-            if (currentMenu == pauseMenu)
+            if (isPaused)
             {
-                isPaused = !isPaused;
-                pauseMenu.SetActive(isPaused);
-                if (isPaused)
-                    gameLockPause();
-                else
-                    gameUnlockPause();
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
             }
         }
     }
-    public void gameLockPause()
+    public void PauseGame()
     {
+        isPaused = true;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
         Time.timeScale = 0;
+        pauseUI.SetActive(true);
     }
-    public void gameUnlockPause()
+    public void ResumeGame()
     {
+        
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         Time.timeScale = 1;
+        isPaused = false;
+        pauseUI.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        EditorApplication.ExitPlaymode();
+        Application.Quit(); 
     }
 }
 
